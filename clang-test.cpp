50b19d6ab121402b50b4077634dc30d61b3ab472
Daniel Bişar <3024016+danielbisar@users.noreply.github.com>
2022-03-19 16:47:34 +0100

    glfw3 build

--- clang-test.cpp
+++ clang-test.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    std::cout << "Hello" << std::endl;
+    std::cout << "init glfw" << std::endl;
+
+    if(!glfwInit())
+    {
+        std::cerr << "failed" << std::endl;
+    }
+
+    glfwTerminate();
+
     return 0;
 }
