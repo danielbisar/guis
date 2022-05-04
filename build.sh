b5204357829e7241e1a5a3b1f5428409a728d6d5
Daniel Bisar <daniel.bisar.extern@jungheinrich.de>
2022-05-04 13:48:19 +0200

    just use default compiler in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 if [ ! -d build ]; then
     mkdir build
     cd build
-    CC=/usr/bin/clang-14 CXX=/usr/bin/clang++-14 cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
+    cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
 else
     cd build
 fi
